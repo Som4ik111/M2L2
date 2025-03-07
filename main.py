e6456192d8311fccad0516e8b08934f98d7ff911
Som4ik111 <ospishchev.mark@gmail.com>
2025-03-07 19:44:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import telebot
 import random
 import os
+from bot_logic import eco
+from bot_logic import ani
 
 
 
-bot = telebot.TeleBot("7808813003:AAHwxaDYLW4NFEwu9tMo3o5TG8mgplO6gOY")
+bot = telebot.TeleBot("#ВАШ ТОКЕН")
 
 
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}! У меня есть команды /start, /mem, /ecology, /animals')
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}! Пропиши /help для более подобной информации!')
+
+
+
+@bot.message_handler(commands=['help'])
+def send_helper(message):
+    bot.reply_to(message, f'Я бот который для обьяснения подросткам важности экологии. У меня есть команды /start, /mem, /ecology, /animals!')
 
 
 
@@ -24,14 +32,13 @@ def send_mem(message):
 
 
 @bot.message_handler(commands=['ecology'])
-def send_welcome(message):
-    bot.reply_to(message, f'Привет, Дорогой друг, ты знаешь какого важность экологии в нашей жизни? А что будет если ближайшие лет 100 люди продолжат выбрасывать пластиковые бутылки в окружающую среюу?: •  Загрязнение океанов: Пластиковые бутылки попадают в реки, а затем в океаны, где они разлагаются на микропластик. Этот микропластик загрязняет воду, оседает на дне, попадает в пищевую цепь и отравляет морских обитателей.•  Загрязнение почвы: На суше пластиковые бутылки загрязняют почву, препятствуя росту растений, нарушая водный баланс и выделяя токсичные вещества при разложении.•  Визуальное загрязнение: Пластиковые отходы портят пейзажи, снижая эстетическую ценность природы.')
-
+def send_ecer(message):
+    bot.reply_to(message, eco())
 
 
 @bot.message_handler(commands=['animals'])
-def send_welcome(message):
-    bot.reply_to(message, f'А ты знаешь, что нужно делать с животными пострадавшими от плохих условий окружающей среды созданых человеком? 1. Немедленно отвози их к ветеренару. 2. По желанию можешь забрать животное себе :), конечно если ты этого хочешь!!!')
+def send_anim134(message):
+    bot.reply_to(message, ani())
 
 
 
